b5442052a85eee67d70d35ee65eabd8ffc54b166
jorispdejong <jorispdejong@gmail.com>
2021-04-30 13:29:57 +0200

    added csv files for recovered and deaths

--- R/DataAnalysis.R
+++ R/DataAnalysis.R
@@ -7,18 +7,25 @@ set.seed(6)
 # source
 source('Functions.R')
 
-# import data
-data_raw <- importData()
-data <- structureData(data_raw)
+# rdata file path
+rdata_file_path <- 'data/RData/data.RData'
+
+if(!file.exists(rdata_file_path)){
+  preprocessData(verbose = F)
+}
+# load RData
+load(rdata_file_path)
 
 # time series for NL
-NL_confirmed <- data$Netherlands$confirmed_cases
-times_confirmed <- data$Netherlands$t
+country <- 181
+NL_confirmed <- data[[country]]$confirmed
+times_confirmed <- data[[country]]$t
 NL_daily <- diff(NL_confirmed)
 times_daily <- times_confirmed[2:length(times_confirmed)]
   
 # plot time series
 par(mfrow=c(2,1), mar=c(3,3,2,2))
-plot(times_confirmed, NL_confirmed, type = 'l', main = 'Cumulative cases', xlab = '', ylab = '')
+plot(times_confirmed, NL_confirmed, type = 'l', 
+     main = paste0('Cumulative infected cases - ', names(data)[country]), xlab = '', ylab = '')
 plot(times_daily, NL_daily, type='l', main = 'Daily cases', xlab = '', ylab = '')
 
--- R/Functions.R
+++ R/Functions.R
@@ -1,62 +1,133 @@
-###################
-### IMPORT DATA ###
-###################
-# download csv file from the John Hopkins university github repo
-# write the csv file to the file 'time_series_covid19_confirmed_global.csv' in a new folder 'csv'
-# if the file already exists then read the data from the file
-importData <- function(verbose = T){
+#####################
+### PREPROCESSING ###
+#####################
+# download csv files from the John Hopkins university github repo
+# write the csv files to the new folder 'csv'
+# if the files already exists then read the data from the files
+# restructure the data into a list
+# save as rdata file
+preprocessData <- function(verbose = T){
   # library
   suppressPackageStartupMessages(library('readr')) # read csv
   
   # folder name + file name and path
   folder_name <- 'csv' 
-  file_name <- 'time_series_covid19_confirmed_global.csv'
-  file_path <- paste0(folder_name, '/', file_name)
   
   # create new folder that will hold all the csv files
   dir.create(folder_name, showWarnings = F) # if it already exists will only throw an error
   
-  if(!file.exists(file_path)){
-    if(verbose) print('Downloading csv files from open john hopkins university github repo...')
+  # file name + path
+  file_name_c <- 'time_series_covid19_confirmed_global.csv'
+  file_path_c <- paste0(folder_name, '/', file_name_c)
+  if(!file.exists(file_path_c)){
+    if(verbose) print('Downloading confirmed global cases from open john hopkins university github repo...')
     
-    # read csv file from web
-    url <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-    data_raw <- read_csv(url, 
-                         col_types = cols(`Province/State` = col_character(), 
-                                          `Country/Region` = col_character()))
-    
+    # confirmed cases - global 
+    url_confirmed <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    confirmed_raw <- read_csv(url_confirmed, 
+                              col_types = cols(`Province/State` = col_character(), 
+                                               `Country/Region` = col_character()))
     # write csv file to folder
-    write.csv(confirmed_global_raw, file = file_path, row.names = F)
+    write.csv(confirmed_raw, file = file_path_c, row.names = F)
   }else{
-    if(verbose) print('Importing csv file from folder')
+    if(verbose) print('Importing confirmed cases file from folder')
     # read csv file from file
-    data_raw <- read_csv(file = file_path, 
-                         col_types = cols(`Province/State` = col_character(), 
-                                          `Country/Region` = col_character()))
+    confirmed_raw <- read_csv(file = file_path_c, 
+                              col_types = cols(`Province/State` = col_character(), 
+                                               `Country/Region` = col_character())) 
   }
   
-  return(data_raw)
+  # file name + path
+  file_name_r <- 'time_series_covid19_recovered_global.csv'
+  file_path_r <- paste0(folder_name, '/', file_name_r)
+  if(!file.exists(file_path_c)){
+    if(verbose) print('Downloading recovered global cases from open john hopkins university github repo...')
+    
+    # recovered cases - global
+    url_recovered <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+    recovered_raw <- read_csv(url_recovered, 
+                              col_types = cols(`Province/State` = col_character(), 
+                                               `Country/Region` = col_character()))
+    # write csv file to folder
+    write.csv(recovered_raw, file = file_path_r, row.names = F)
+  }else{
+    if(verbose) print('Importing recovered cases file from folder')
+    # read csv file from file
+    recovered_raw <- read_csv(file = file_path_r, 
+                              col_types = cols(`Province/State` = col_character(), 
+                                               `Country/Region` = col_character())) 
+  }
+  
+  # file name + path
+  file_name_d <- 'time_series_covid19_deaths_global.csv'
+  file_path_d <- paste0(folder_name, '/', file_name_d)
+  if(!file.exists(file_path_c)){
+    if(verbose) print('Downloading deaths global cases from open john hopkins university github repo...')
+    
+    # deaths cases - global
+    url_deaths <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    deaths_raw <- read_csv(url_deaths, 
+                           col_types = cols(`Province/State` = col_character(), 
+                                            `Country/Region` = col_character()))
+    # write csv file to folder
+    write.csv(deaths_raw, file = file_path_d, row.names = F)
+  }else{
+    if(verbose) print('Importing deaths cases file from folder')
+    # read csv file from file
+    deaths_raw <- read_csv(file = file_path_c, 
+                           col_types = cols(`Province/State` = col_character(), 
+                                            `Country/Region` = col_character())) 
+  }
+  if(verbose) print('Done!')
+  data_raw <- list('confirmed'=confirmed_raw,
+                   'recovered'=recovered_raw,
+                   'deaths'=deaths_raw)
+  
+  # structure the data
+  data <- structureData(data_raw, verbose = verbose)
+  
+  # save to RData file
+  save(data, file=rdata_file_path)
 }
 
 ######################
 ### STRUCTURE DATA ###
 ######################
 # create a structured list of the data
-structureData <- function(data_raw){
-  # reformat data into list
-  data <- apply(as.matrix(data_raw), 1, 
-                function(row){
-                  lat <- as.numeric(row[3])
-                  lon <- as.numeric(row[4])
-                  confirmed_cases <- as.numeric(row[5:length(row)])
-                  t <- as.Date(names(row[5:length(row)]), '%m/%d/%y')
-                  
-                  return(list('lat'=lat,
-                              'lon'=lon,
-                              'confirmed_cases'=confirmed_cases,
-                              't'= t))
-                })
-  names(data) <- apply(data_raw, 1, function(row) if(is.na(row[1])) row[2] else paste0(row[2], '-', row[1]))
+structureData <- function(data_raw, verbose=T){
+  if(verbose) print('Restructuring the data to a list...')
+  # get common countries from the csv files
+  common_countries <- Reduce(intersect,
+                             sapply(1:length(data_raw),
+                                    function(i)
+                                      apply(data_raw[[i]], 1,
+                                            function(row)
+                                              if(is.na(row[1])) row[2] else paste0(row[2], '-', row[1]))))
+  
+  # reformat data to list
+  data <- lapply(1:length(data_raw$confirmed), 
+                 function(i){
+                   row_c <- data_raw$confirmed[i,]
+                   row_r <- data_raw$recovered[i,]
+                   row_d <- data_raw$deaths[i,]
+                   
+                   lat <- as.numeric(row_c[3])
+                   lon <- as.numeric(row_c[4])
+                   t <- as.Date(names(row_c[5:length(row_c)]), '%m/%d/%y')
+                   confirmed <- as.numeric(row_c[5:length(row_c)])
+                   recovered <- as.numeric(row_r[5:length(row_r)])
+                   deaths <- as.numeric(row_d[5:length(row_d)])
+                   
+                   return(list('lat'=lat,
+                               'lon'=lon,
+                               'confirmed'=confirmed,
+                               'recovered'=recovered,
+                               'deaths'=deaths,
+                               't'= t))
+                 })
+  names(data) <- common_countries
+  
+  if(verbose) print('Done!')
   
   return(data)
 }
